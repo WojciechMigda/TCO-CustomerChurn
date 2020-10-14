2fed349acfb5af83ade3f396e24696307407e31c
Wojciech Migda <wojtek.golf@interia.pl>
2020-10-14 20:50:18 +0200

    Add EDA

---
+++ src/eda_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+# A1 Austria
+
+
+def read_train():
+    import pandas as pd
+
+    df = pd.read_csv('../data/input.csv', na_values='*******',
+#            nrows=100000
+        )
+    print('Full TRAIN shape: {}'.format(df.shape))
+
+    return df
+
+
+def main():
+    import pandas as pd
+
+    df = read_train()
+
+
+    # iterate over columns
+    for colname in df:
+        col = df[colname]
+        has_nans = 'NaN' if col.isnull().values.any() else ''
+        uniq_vals = col.nunique(dropna=True)
+
+        min_ = col.min() if col.dtype.name != 'object' else '---'
+        max_ = col.max() if col.dtype.name != 'object' else '---'
+
+        obj_vals = ','.join( list(set(col.dropna().values))[:10] ) if col.dtype.name == 'object' else ''
+
+        print('{:>30s} {:>8s} {:>3s} {:>8d} {:>8s} {:>8s} {}'.format(colname, col.dtype.name, has_nans, uniq_vals, str(min_), str(max_), obj_vals))
+
+    return
+
+
+if __name__ == "__main__":
+    main()
