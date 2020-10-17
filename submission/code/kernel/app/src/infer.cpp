6980ff0f7baafaf41ac191baae3e762f951a7cad
Wojciech Migda <wojtek.golf@interia.pl>
2020-10-17 17:40:44 +0200

    Put some initial code into infer()

--- submission/code/kernel/app/src/infer.cpp
+++ submission/code/kernel/app/src/infer.cpp
@@ -1,11 +1,18 @@
-#include <string>
+#include "read_csv.hpp"
 
 #include "json/json.hpp"
+#include "spdlog/spdlog.h"
+
+#include <string>
+
 
 void infer(
     std::string const & csv_ifname,
     std::string const & encoder_ifname,
     std::string const & model_ifname)
 {
+    auto [cat_rows, num_rows] = read_csv(csv_ifname);
 
+    spdlog::info("CSV: read {} rows with categorical features and {} rows with numerical ones.",
+        cat_rows.size(), num_rows.size());
 }
