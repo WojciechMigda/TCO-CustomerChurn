0cfa826a776a11ecdaccb2faa96a02f467514130
Wojciech Migda <wojtek.golf@interia.pl>
2020-10-19 01:15:25 +0200

    Propagate some train() code to infer()

--- submission/code/kernel/app/src/infer.cpp
+++ submission/code/kernel/app/src/infer.cpp
@@ -1,4 +1,6 @@
 #include "read_csv.hpp"
+#include "json_io.hpp"
+#include "data_tools.hpp"
 
 #include "json/json.hpp"
 #include "spdlog/spdlog.h"
@@ -11,8 +13,26 @@ void infer(
     std::string const & encoder_ifname,
     std::string const & model_ifname)
 {
+    auto encoding = read_json(encoder_ifname);
+
+    if (encoding.empty())
+    {
+        spdlog::error("JSON: encoding read from {} failed.", encoder_ifname);
+
+        return;
+    }
+
     auto [cat_rows, num_rows] = read_csv(csv_ifname);
+
+    if (cat_rows.empty() or num_rows.empty())
+    {
+        spdlog::error("CSV: read from {} failed.", csv_ifname);
+
+        return;
+    }
 
     spdlog::info("CSV: read {} rows with categorical features and {} rows with numerical ones.",
         cat_rows.size(), num_rows.size());
+
+    auto X_train = encode_features(cat_rows, num_rows, encoding);
 }
