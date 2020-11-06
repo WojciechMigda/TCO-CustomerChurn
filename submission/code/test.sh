3fa688bf4dc64f2e31e6869b02e785d065bb502d
Wojciech Migda <wojtek.golf@interia.pl>
2020-11-06 13:16:16 +0100

    Submission 15

--- submission/code/test.sh
+++ submission/code/test.sh
@@ -5,7 +5,7 @@ OUT_DIR="${2}"
 
 #./infer.sh "${DATA_DIR}" "${OUT_DIR}" _regr_17_18.model regr1.scores
 
-./infer.sh "${DATA_DIR}" "${OUT_DIR}" _clas_F_17.model clas1.scores
+./infer.sh "${DATA_DIR}" "${OUT_DIR}" _clas_G_19.model clas1.scores
 
 
 ./merge_scores.sh "${OUT_DIR}"/clas1.scores > "${OUT_DIR}"/scores.txt
